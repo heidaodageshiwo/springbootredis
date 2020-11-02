cab74216ab3cd718e55edd9e3178845882cc897a
zhangqiang <heidaodageshiwo@163.com>
2020-11-02 19:27:10 +0800

    张强springbootRedis

---
+++ src/main/java/com/zhangqiangredis/redistest/springbootcontroller/test.java
@@ -0,0 +1,19 @@
+package com.zhangqiangredis.redistest.springbootcontroller;
+
+/**
+ * java类简单作用描述
+ *
+ * @ProjectName: redistest
+ * @Package: com.zhangqiangredis.redistest.springbootcontroller
+ * @ClassName: test
+ * @Description: java类作用描述
+ * @Author: zhangq
+ * @CreateDate: 2020-11-02 19:21
+ * @UpdateUser: zhangq
+ * @UpdateDate: 2020-11-02 19:21
+ * @UpdateRemark: The modified content
+ * @Version: 1.0 *
+ */
+public class test {
+
+}
